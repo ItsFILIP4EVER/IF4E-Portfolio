56fa7d46b2bcfeb9e0be7c02d0eb59d080183b9f
ItsFILIP4EVER <65853073+ItsFILIP4EVER@users.noreply.github.com>
2023-02-15 11:11:18 +0100

    k

--- src/layouts/Home/Home.js
+++ src/layouts/Home/Home.js
@@ -69,8 +69,7 @@ export const Home = () => {
     <div className={styles.home}>
       <Meta
         title="Designer + Developer"
-        description="Design portfolio of Hamish Williams — a product designer working on web & mobile
-          apps with a focus on motion, experience design, and accessibility."
+        description="Portfolio of ItsFILIP4EVER"
       />
       <Intro
         id="intro"
